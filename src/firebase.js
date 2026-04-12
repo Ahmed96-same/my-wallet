fa9a396907e7bf23d95ea75d3544af21a7c35258
Ahmed96-same <83510516+Ahmed96-same@users.noreply.github.com>
2026-04-12 16:32:01 +0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvtqTu3A5as2IZCe4nONdyYq6Ntt4O1uw",
+  authDomain: "my-wallet-458f7.firebaseapp.com",
+  projectId: "my-wallet-458f7",
+  storageBucket: "my-wallet-458f7.firebasestorage.app",
+  messagingSenderId: "1009616809733",
+  appId: "1:1009616809733:web:1fa37c3bfeff9877d7d16c"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db  = getFirestore(app);
+export const auth = getAuth(app);
