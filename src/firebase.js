0d889236ba8a48bb7375bd4e85c7d69100924b6d
Ahmed96-same <83510516+Ahmed96-same@users.noreply.github.com>
2026-04-12 23:15:22 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBvtqTu3A5as2IZCe4nONdyYq6Ntt4O1uw",
@@ -11,6 +11,9 @@ const firebaseConfig = {
   appId: "1:1009616809733:web:1fa37c3bfeff9877d7d16c"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db  = getFirestore(app);
+const app  = initializeApp(firebaseConfig);
+export const db   = getFirestore(app);
 export const auth = getAuth(app);
+
+// حفظ الجلسة محلياً حتى لا تضيع عند إغلاق التطبيق
+setPersistence(auth, browserLocalPersistence).catch(() => {});
